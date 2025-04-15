a1f896f8c5c2ce28d6d2c67829c14dbdeb415a84
Ucokgagah <rudikurniaalamin65@gmail.com>
2025-04-15 12:16:20 +0700

    Container dan Decorations

--- styling_widget/lib/main.dart
+++ styling_widget/lib/main.dart
@@ -1,34 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp()); 
-  }
+
+runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const MyApp({super.key});
 
 @override
 Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Styling Text App',
-    home: Scaffold(
-      appBar:AppBar(
-        title: const Text('Styling Text'),
-        backgroundColor: Colors.blue,
-        ),
-body: const Center(
-  child: Text(
-    'Hello Rudi Kurnia Al Amin 65',
-    style: TextStyle(
-      fontSize: 24,
-      fontWeight: FontWeight.bold,
-      color: Colors.red,
-      decoration: TextDecoration.underline,
-      fontStyle: FontStyle.italic
-      ),
-      ),
-      ),
-      ),
-      );
-      }
-      }
+return MaterialApp(
+title: 'Container Decorations Demo',
+home: Scaffold(
+appBar: AppBar(
+title: const Text('Container & Decorations'),
+backgroundColor: Colors.orange, ),
+body: Center(
+child: Container(
+margin: const EdgeInsets.all(20),
+padding: const EdgeInsets.all(20),
+decoration: BoxDecoration(
+color: Colors.amber,
+borderRadius: BorderRadius.circular(12),
+boxShadow: [
+const BoxShadow(
+color: Colors.grey,
+blurRadius: 5,
+),
+],
+),
+child: const Text('Ini Container dengan dekorasi'),
+),
+),
+),
+);
+}
+}
