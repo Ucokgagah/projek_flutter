9819ec720999845b42a67affc2e07ff4bae84d3b
Ucokgagah <rudikurniaalamin65@gmail.com>
2025-04-15 14:08:24 +0700

    menambahkan ikon

--- styling_widget/lib/main.dart
+++ styling_widget/lib/main.dart
@@ -1,43 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
+  const MyApp({super.key});
 
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: 'Stack Widget Demo',
-home: Scaffold(
-appBar: AppBar(
-title: const Text('Stack Widget'),
-backgroundColor: Colors.green,
-),
-body: Center(
-child: Stack(
-alignment: Alignment.center,
-children: [
-Container(
-width: 200,
-height: 200,
-color: Colors.green,
-),
-const Text(
-'Di Atas Kotak',
-style: TextStyle(
-color: Colors.white,
-fontSize: 20,
-),
-),
-],
-),
-),
-),
-);
-
-}
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stack Widget Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stack Widget'),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.green,
+              ),
+              const Text(
+                'Di Atas Kotak',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              const Positioned(
+                right: 8, // jarak dari kanan
+                bottom: 8, // jarak dari bawah
+                child: Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
