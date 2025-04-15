21ed6174175666608cc4459b31a00df0870c7865
Ucokgagah <rudikurniaalamin65@gmail.com>
2025-04-15 14:01:27 +0700

    stack widget

--- styling_widget/lib/main.dart
+++ styling_widget/lib/main.dart
@@ -1,43 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Container Decorations Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Container & Decorations'),
-          backgroundColor: Colors.orange,
-        ),
-        body: Center(
-          child: Container(
-            margin: const EdgeInsets.all(20),
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: BorderRadius.circular(20), //diubah jadi 20
-              border: Border.all(
-                color: Colors.blue, //border biru
-                width: 2,
-              ),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 5,
-                ),
-              ],
-            ),
-            child: const Text('Ini Container dengan dekorasi'),
-          ),
-        ),
-      ),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+title: 'Stack Widget Demo',
+home: Scaffold(
+appBar: AppBar(
+title: const Text('Stack Widget'),
+backgroundColor: Colors.green,
+),
+body: Center(
+child: Stack(
+alignment: Alignment.center,
+children: [
+Container(
+width: 200,
+height: 200,
+color: Colors.green,
+),
+const Text(
+'Di Atas Kotak',
+style: TextStyle(
+color: Colors.white,
+fontSize: 20,
+),
+),
+],
+),
+),
+),
+);
+
+}
+
 }
