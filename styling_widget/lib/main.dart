820c3d47252b24c09c0e8f40424101b1b778d8e6
Ucokgagah <rudikurniaalamin65@gmail.com>
2025-04-15 14:42:47 +0700

    menambahkan tema gelap dan merubah warna app bar

--- styling_widget/lib/main.dart
+++ styling_widget/lib/main.dart
@@ -1,43 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
+  const MyApp({super.key});
 
-@override
-Widget build (BuildContext context) {
-return MaterialApp(
-title: 'Flutter Theme Demo',
-theme: ThemeData(
-primarySwatch: Colors.teal,
-textTheme: const TextTheme(
-bodyLarge: TextStyle(
-fontSize: 18,
-color: Colors.black,
-),
-),
-),
-home: const MyHomePage(),
-);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Theme Demo',
+      theme: ThemeData.dark().copyWith( //Aktifkan dark mode
+        primaryColor: Colors.deepPurple,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.deepPurple, //Ubah warna AppBar
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(
+            fontSize: 18,
+            color: Colors.white, //Warna teks disesuaikan untuk dark mode
+          ),
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
 }
-}
+
 class MyHomePage extends StatelessWidget {
-const MyHomePage({super.key});
+  const MyHomePage({super.key});
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: const Text('Home with Theme'),
-),
-body: Center(
-child: Text('Ini teks dengan tema!',
-style: Theme.of(context).textTheme.bodyLarge, 
-),
-),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home with Theme'),
+      ),
+      body: Center(
+        child: Text(
+          'Ini teks dengan tema!',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ),
+    );
+  }
 }
