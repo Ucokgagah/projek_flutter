ba2dc82c7ae3990544d6152aaf1c2b71fa54fdf2
Ucokgagah <rudikurniaalamin65@gmail.com>
2025-04-15 12:56:11 +0700

    menambahkan border dan radius 20

--- styling_widget/lib/main.dart
+++ styling_widget/lib/main.dart
@@ -1,39 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
-runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
+  const MyApp({super.key});
 
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: 'Container Decorations Demo',
-home: Scaffold(
-appBar: AppBar(
-title: const Text('Container & Decorations'),
-backgroundColor: Colors.orange, ),
-body: Center(
-child: Container(
-margin: const EdgeInsets.all(20),
-padding: const EdgeInsets.all(20),
-decoration: BoxDecoration(
-color: Colors.amber,
-borderRadius: BorderRadius.circular(12),
-boxShadow: [
-const BoxShadow(
-color: Colors.grey,
-blurRadius: 5,
-),
-],
-),
-child: const Text('Ini Container dengan dekorasi'),
-),
-),
-),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Container Decorations Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Container & Decorations'),
+          backgroundColor: Colors.orange,
+        ),
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(20), //diubah jadi 20
+              border: Border.all(
+                color: Colors.blue, //border biru
+                width: 2,
+              ),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 5,
+                ),
+              ],
+            ),
+            child: const Text('Ini Container dengan dekorasi'),
+          ),
+        ),
+      ),
+    );
+  }
 }
